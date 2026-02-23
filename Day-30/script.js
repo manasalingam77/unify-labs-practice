7024293218efd22f8959c3b7dbc0f484489fa6e5
Manasa Lingam <manasalingam44@gmail.com>
2026-02-23 19:40:06 +0530

    🚀 Day 30 - index.html - 7:40:02 pm

---
+++ Day-30/script.js
@@ -0,0 +1,7 @@
+// Day 30 Logic Practice
+console.log('Lab Session 30 Started');
+
+const display = document.getElementById('display');
+display.innerText = 'Logic Engine Online';
+
+// Practice your JS code here...
